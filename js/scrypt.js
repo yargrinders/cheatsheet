1c92a4ce6b290a8b6289ea652aec46aafcdb21dd
yargrinders <yargrindersp20@gmail.com>
2022-04-04 13:37:11 +0300

    v.1

---
+++ js/scrypt.js
@@ -0,0 +1,22 @@
+var tabNavs = document.querySelectorAll(".nav-tab");
+var tabPanes = document.querySelectorAll(".tab-pane");
+
+for (var i = 0; i < tabNavs.length; i++) {
+
+tabNavs[i].addEventListener("click", function(e){
+    e.preventDefault();
+    var activeTabAttr = e.target.getAttribute("data-tab");
+
+    for (var j = 0; j < tabNavs.length; j++) {
+    var contentAttr = tabPanes[j].getAttribute("data-tab-content");
+
+    if (activeTabAttr === contentAttr) {
+        tabNavs[j].classList.add("active");
+        tabPanes[j].classList.add("active"); 
+    } else {
+        tabNavs[j].classList.remove("active");
+        tabPanes[j].classList.remove("active");
+    }
+    };
+});
+}
