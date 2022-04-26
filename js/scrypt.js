800ce02827b6471efcddd909867dbf95926adc17
yargrinders <yargrindersp20@gmail.com>
2022-04-26 08:47:45 +0200

    Burger v.1

--- js/scrypt.js
+++ js/scrypt.js
@@ -20,3 +20,13 @@ tabNavs[i].addEventListener("click", function(e){
     };
 });
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
